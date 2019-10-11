5715c845b110f217d99d776fe4d345ef9113a601
pavanreddy4227 <54386960+pavanreddy4227@users.noreply.github.com>
2019-10-11 15:23:28 -0500

    sdfhjk

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
-
-
+const add = (x,y)=>{return x+y}
+const sub=(x,y)=>{return x-y}
+const mul=(x,y)=>{return x*y}
 const updateWithAdd = async (event) => {
   document.querySelector('#result').innerHTML = ''
   if (document.querySelector('#firstNumber').checkValidity() && document.querySelector('#secondNumber').checkValidity()) {
   
     const i = parseInt(document.querySelector('#firstNumber').value)
     const j = parseInt(document.querySelector('#secondNumber').value)
-    const ans = i+j;
-    const ans1=i-j;
-    const ans2=i*j;
-    var a="Sum is:"+ans+"\n"+ "Subraction is:"+ans1+"\n"+" multiplication is:"+ans2
-    document.querySelector('#result').innerHTML = a
+    const ans =`Sum is ${add(i,j)}.`
+    const ans1 =`difference is ${sub(i,j)}.`
+    const ans2 =`mul is ${mul(i,j)}.`
+    document.querySelector('#result').innerHTML = ans+"\n"+ans1+"\n"+ans2
    // document.querySelector('#result').innerHTML = "Difference is:"+ans1
    // document.querySelector('#result').innerHTML = "Multiple is:"+ans2
   }

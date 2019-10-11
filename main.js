04a4d4cc09fed0cb6e82f150bba19fba350d193d
pavanreddy4227 <54386960+pavanreddy4227@users.noreply.github.com>
2019-10-10 22:57:19 -0500

    dfgh

--- main.js
+++ main.js
@@ -9,7 +9,8 @@ const updateWithAdd = async (event) => {
     const ans = i+j;
     const ans1=i-j;
     const ans2=i*j;
-    document.querySelector('#result').innerHTML = "Sum is:"+ans+"\n"+ "Subraction is:"+ans1+"\n"+" multiplication is:"+ans2
+    var a="Sum is:"+ans+"\n"+ "Subraction is:"+ans1+"\n"+" multiplication is:"+ans2
+    document.querySelector('#result').innerHTML = a
    // document.querySelector('#result').innerHTML = "Difference is:"+ans1
    // document.querySelector('#result').innerHTML = "Multiple is:"+ans2
   }

71ad95b0013b0b0df46a611aed98591b37bc6973
pavanreddy4227 <54386960+pavanreddy4227@users.noreply.github.com>
2019-10-11 17:00:14 -0500

    dfghj

--- main.js
+++ main.js
@@ -1,26 +1,28 @@
-const add = (x,y)=>{return x+y}
-const updateWithAdd = async (event) => {
+const add1 = (x,y)=>{return x+y}
+const updateWithAdd1 = async (event) => {
+  
   document.querySelector('#result').innerHTML = ''
   if (document.querySelector('#firstNumber').checkValidity() && document.querySelector('#secondNumber').checkValidity()) {
   
     const i = parseInt(document.querySelector('#firstNumber').value)
     const j = parseInt(document.querySelector('#secondNumber').value)
-    const ans =`Sum is ${add(i,j)}.`
+    const ans =`Sum is ${add1(i,j)}.`
     document.querySelector('#result').innerHTML = ans
    // document.querySelector('#result').innerHTML = "Difference is:"+ans1
    // document.querySelector('#result').innerHTML = "Multiple is:"+ans2
   }
-  
-const getAdvice = async () =>{
-  var data = await fetch("https://api.adviceslip.com/advice")
-  var json = await data.json()
-  console.log(json.slip.advice)
-  document.querySelector("#adviceField").innerHTML = json.slip.advice
+  const updateWithJoke = async (event) => {
+    document.querySelector('#joker').innerHTML = ''
+    const url = 'https://api.icndb.com/jokes/random?limitTo=[nerdy]'
+    const response = await fetch(url)
+    const obj = await response.json()
+    const joke = obj.value.joke || 'No joke for you.'
+    document.querySelector('#joker').innerHTML = joke
+  }
+  document.addEventListener('click', event => {
+    if (event.target && event.target.id === 'getJokeButton') { updateWithJoke(event) }
+  })
 }
-  
-}
-
-
 document.addEventListener('click', event => {
-  if (event.target && event.target.id === 'addButton') { updateWithAdd(event) }
+  if (event.target && event.target.id === 'addButton') { updateWithAdd1(event) }
 })

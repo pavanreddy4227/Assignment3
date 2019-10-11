fd3406ef44cce3619a15ac972047a29976cfc66d
pavanreddy4227 <54386960+pavanreddy4227@users.noreply.github.com>
2019-10-11 16:30:28 -0500

    dfghj

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
 const add = (x,y)=>{return x+y}
-const sub=(x,y)=>{return x-y}
-const mul=(x,y)=>{return x*y}
 const updateWithAdd = async (event) => {
   document.querySelector('#result').innerHTML = ''
   if (document.querySelector('#firstNumber').checkValidity() && document.querySelector('#secondNumber').checkValidity()) {
@@ -8,12 +6,17 @@ const updateWithAdd = async (event) => {
     const i = parseInt(document.querySelector('#firstNumber').value)
     const j = parseInt(document.querySelector('#secondNumber').value)
     const ans =`Sum is ${add(i,j)}.`
-    const ans1 =`\ndifference is ${sub(i,j)}.`
-    const ans2 =`\nmul is ${mul(i,j)}.`
-    document.querySelector('#result').innerHTML = ans+"\n"+ans1+"\n"+ans2
+    document.querySelector('#result').innerHTML = ans
    // document.querySelector('#result').innerHTML = "Difference is:"+ans1
    // document.querySelector('#result').innerHTML = "Multiple is:"+ans2
   }
+  
+const getAdvice = async () =>{
+  var data = await fetch("https://api.adviceslip.com/advice")
+  var json = await data.json()
+  console.log(json.slip.advice)
+  document.querySelector("#adviceField").innerHTML = json.slip.advice
+}
   
 }
 
--- test/main.test.js
+++ test/main.test.js
@@ -1,51 +1,14 @@
 QUnit.module('MAIN MODULE', {})  // group all these tests together
 
 QUnit.test('TEST add', assert => {
-  assert.equal(add(1, 1), 2, 'Positive integers')
-  assert.equal(add(-1, -1), -2, 'Negative integers')
-  assert.equal(add(-10, 10), 0, 'Mixed')
+  assert.equal(add(3, 2), 7, 'correct')
+  assert.equal(add(-1, -1), -3, 'correct')
+  assert.equal(add(-10, 10), 10, 'correct')
+  assert.equal(add(-1, 10), 19, 'correct')
+  assert.equal(add(0, 10), 20, 'correct')
 })
 
-QUnit.config.autostart = false  // sync = false; start after loading html
-
-// This script, called when the page loads, reaches out to the app that we wish to test
-// It basically pastes the contents of that page into *this* web page, whew! This shows
-// how we manipulate the DOM.
-
-// The openingTag and closingTag specify which part of the original app's web page that we grab here
-// Pretty slick eh?
-
-window.addEventListener('load', () => {
-  const appURL = '../index.html' // reach out to the html for the app (js-gui)
-  const openingTag = '<main class="container mt-5 flex-fill">'
-  const closingTag = '</main>' // go grab it!
-  fetch(appURL, { method: 'GET' })
-    .then(response => {
-      return response.text() // returns promise
-    })
-    .then(txt => {                
-      const start = txt.indexOf(openingTag)
-      const end = txt.indexOf(closingTag) + closingTag.length
-      const html = txt.substring(start, end) // we only want part of the page
-      const qunitFixtureBody = document.querySelector('#qunit-fixture')
-      qunitFixtureBody.innerHTML = html // put the page into the DOM - the second div associated with this page
-      console.info(qunitFixtureBody) // print it out so we can see it (it doesn't get inserted into the page)
-      QUnit.start() // start the actual testing - it finds and runs both the tests, defined in QUnit.test()
-    })
-    .catch(error => {
-      console.error('error:', error);
-      QUnit.start()
-    })
+window.addEventListener('Load', ()=>
+{
+  Qunit.start()
 })
-
-QUnit.test("TEST first number validation", assert => {
-  const input = document.querySelector('#firstNumber')
-  const warning = document.querySelector('#firstWarning')
-  input.value = -3;
-  assert.equal(input.value, -3, "Bad value assigned")
-  assert.strictEqual(input.checkValidity(), false, "Correctly fails validation")
-  input.focus()
-  input.blur()
-  assert.strictEqual(warning.innerHTML, 'Invalid input', `Correctly adds warning ${warning}`)
-})
-
